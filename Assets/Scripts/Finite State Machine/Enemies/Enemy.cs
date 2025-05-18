fb27cde9a5eae54c55484ba65f589fb828fc8830
Fouad Elamoury <foad.osama@hotmail.com>
2025-05-18 16:52:19 +0300

    Enemy States
    inserting Enemy States in Enemy script

--- Assets/Scripts/Finite State Machine/Enemies/Enemy.cs
+++ Assets/Scripts/Finite State Machine/Enemies/Enemy.cs
@@ -5,6 +5,9 @@ public class Enemy : MonoBehaviour
     [SerializeField] EnemyData enemyData;
     public StateMachine enemyStateMachine {  get; private set; }
     public Enemy_IdleState enemyIdleState {get; private set; }
+    public Enemy_AttackState enemyAttackState {get; private set; }
+    public Enemy_ChaseState enemyChaseState {get; private set; }
+    public Enemy_PatrolState enemyPatrolState {get; private set; }
     private void Awake()
     {
         enemyStateMachine = new StateMachine();
