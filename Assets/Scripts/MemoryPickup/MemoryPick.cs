24727d4eafc5fa0d9b05033574cf33dfe9b77c6d
NabilaKhalil124 <nabilahossaeldenissa@gmail.com>
2025-05-20 21:59:33 +0300

    Display a UI message when the wrong memory is picked

--- Assets/Scripts/MemoryPickup/MemoryPick.cs
+++ Assets/Scripts/MemoryPickup/MemoryPick.cs
@@ -1,8 +1,11 @@
 using System.Buffers;
+using System.Collections;
 using UnityEngine;
 
 public class MemoryPick : PickupBase
 {
+    [SerializeField] GameObject WrongMemoryMessage;
+    [SerializeField] float messageDuration = 3.0f;
     public override void Pickup(GameObject player)
     {
         if (MemoryManager.Instance != null)
@@ -15,8 +18,26 @@ public class MemoryPick : PickupBase
             else
             {
                 Debug.LogWarning($"Wrong memory: {name}. Pick the right one.");
+                ShowWrongMemoryMessage();
             }
         }
     }
 
+    private void ShowWrongMemoryMessage()
+    {
+        if (WrongMemoryMessage != null)
+        {
+            WrongMemoryMessage.SetActive(true);
+            StartCoroutine(HideMessageAfterDelay());
+        }
+    }
+
+    private IEnumerator HideMessageAfterDelay()
+    {
+        yield return new WaitForSeconds(messageDuration);
+        if (WrongMemoryMessage != null)
+        {
+            WrongMemoryMessage.SetActive(false);
+        }
+    }
 }
