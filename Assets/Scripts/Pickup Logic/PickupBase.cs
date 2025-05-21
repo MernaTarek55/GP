ad5d1a382e67c1743c5e0824fff30db13c68590b
MernaTarek55 <91115322+MernaTarek55@users.noreply.github.com>
2025-05-21 13:32:17 +0300

    just a merge

--- Assets/Scripts/Pickup Logic/PickupBase.cs
+++ Assets/Scripts/Pickup Logic/PickupBase.cs
@@ -7,8 +7,19 @@ public abstract class PickupBase : MonoBehaviour, IPickupInterface
     {
         if (other.CompareTag("Player"))
         {
+            Debug.Log("trigger enter ");
             Pickup(other.gameObject);
             //Destroy(gameObject);
         }
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("collision enter ");
+            Pickup(collision.gameObject);
+            //Destroy(gameObject);
+        }
+    }
+    
 }
