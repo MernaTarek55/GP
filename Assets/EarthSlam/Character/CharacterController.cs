b35ef6387dcf3ac683a206849d25655931f07eab
MernaTarek55 <91115322+MernaTarek55@users.noreply.github.com>
2025-05-26 10:18:09 +0300

    add slam to enemy

---
+++ Assets/EarthSlam/Character/CharacterController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] Animator _Anim;
+    [SerializeField] CrackControll _CrackPrefab;
+    Vector3 direction;
+   
+    public void CharacterAttack (){
+    direction = transform.forward;
+            _Anim.SetTrigger("Attack");
+}
+    private void AnimationCallback_SlamEffect()
+    {
+        Vector3 pos = transform.position;
+        pos.y = 0;
+        CrackControll crackControll = Instantiate(_CrackPrefab, pos, Quaternion.identity);
+        Destroy(crackControll.gameObject, 3f);
+        crackControll.transform.forward = direction;
+        crackControll.Open(15);
+    }
+}
