9da46bec3589126c4f6e2b96ab6013e9148ff1cb
NabilaKhalil124 <nabilahossaeldenissa@gmail.com>
2025-05-28 19:24:54 +0300

    Grenade Launcher Weapon + Shell bullet

---
+++ Assets/Scripts/Enemy Bullet/Bullet.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public float lifeTime = 2f;
+
+    private void OnEnable()
+    {
+        Invoke(nameof(Disable), lifeTime);
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    void Disable()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            other.gameObject.GetComponent<HealthComponent>().TakeDamage(10);
+        if (other.gameObject.CompareTag("Enemy"))
+            other.gameObject.GetComponent<HealthComponent>().TakeDamage(10);
+        Disable();
+    }
+}
