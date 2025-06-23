702517037bbf912cfaf1e7237ee1794d2b209435
Omar Tantawy <o.tantawy.o@gmail.com>
2025-06-23 16:04:57 +0300

    Testing

--- Assets/Scripts/Finite State Machine/Player/Player_JumpState.cs
+++ Assets/Scripts/Finite State Machine/Player/Player_JumpState.cs
@@ -14,37 +14,29 @@ public class Player_JumpState : EntityState
 
         if (player.IsGrounded && !player.hasJumped)
         {
-            player.animator.Update(0f);
-            int rocketFlyHash = Animator.StringToHash("RocketFly");
-            player.animator.Play(rocketFlyHash);
             player.rb.AddForce(Vector3.up * 5f, ForceMode.Impulse);
             player.hasJumped = true;
         }
 
-        player.animator.SetBool("IsFlying", true);
-        player.animator.SetBool("Grounded", false);
+        player.animator.SetBool("IsJumping", true); // Use if needed to control transitions
     }
 
     public override void Update()
     {
         base.Update();
 
-        if (!player.IsGrounded && player.rb.linearVelocity.y < 0)
-        {
-            player.animator.SetBool("IsFlying", true);
-        }
+        // Update vertical velocity for the blend tree
+        float verticalVelocity = player.rb.linearVelocity.y;
+        player.animator.SetFloat("VerticalVelocity", verticalVelocity);
 
         if (player.IsGrounded && player.hasJumped)
         {
             player.hasJumped = false;
+
             if (player.healthComponent.IsDead())
             {
                 stateMachine.ChangeState(player.playerDeath);
             }
-            //else if (player.DeadEyePressed)
-            //{
-            //    stateMachine.ChangeState(new Player_DeadEyeStateTest1(stateMachine, "DeadEye", player));
-            //}
             else if (player.MoveInput.sqrMagnitude > 0.01f)
             {
                 var moveState = new Player_MoveState(stateMachine, "Move", player);
@@ -58,6 +50,7 @@ public class Player_JumpState : EntityState
             }
         }
     }
+
 
     public override void Exit()
     {
