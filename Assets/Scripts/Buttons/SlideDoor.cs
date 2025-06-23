bf2ac7d1d5b8ab58901380ca3dca8748d5c7c544
NabilaKhalil124 <nabilahossaeldenissa@gmail.com>
2025-06-23 19:06:40 +0300

    level 3 with textures

--- Assets/Scripts/Buttons/SlideDoor.cs
+++ Assets/Scripts/Buttons/SlideDoor.cs
@@ -2,24 +2,29 @@ using DG.Tweening;
 using UnityEngine;
 public class SlideDoor : MonoBehaviour
 {
-    [SerializeField] private float moveAmount = 2;
+    //[SerializeField] private float moveAmount = 2;
+    [SerializeField] private Vector3 rotationAmount = new Vector3(0,90,0);
     [SerializeField] private float duration = 1;
-    [SerializeField] private Vector3 endPos;
-    private Vector3 startPos;
+    //[SerializeField] private Vector3 endPos;
+    private Vector3 startRotation;
+    //private Vector3 startPos;
     private void Awake()
     {
-        startPos = transform.localPosition;
+        startRotation = transform.localEulerAngles;
     }
 
     [ContextMenu("Open")]
     public void Open()
     {
-        transform.DOLocalMove(transform.localPosition + endPos, duration);
+        //transform.DOLocalMove(transform.localPosition + endPos, duration);
+        transform.DOLocalRotate(startRotation + rotationAmount, duration);
     }
 
     [ContextMenu("Close")]
     public void Close()
     {
-        transform.DOLocalMove(startPos, duration);
+        //transform.DOLocalMove(startPos, duration);
+        transform.DOLocalRotate(startRotation, duration);
+
     }
 }
