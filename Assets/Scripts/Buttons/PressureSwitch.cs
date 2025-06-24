c1dd3888dde3f910fadd8576c39b881e05baa922
NabilaKhalil124 <nabilahossaeldenissa@gmail.com>
2025-06-24 08:28:20 +0300

    buttons for moving platform puzzle and doors

--- Assets/Scripts/Buttons/PressureSwitch.cs
+++ Assets/Scripts/Buttons/PressureSwitch.cs
@@ -8,29 +8,45 @@ public class PressureSwitch : MonoBehaviour
         Hold
     }
 
-    [SerializeField] private Door currentDoor;
+    public enum ButtonBehaviourTarget
+    {
+        Door ,
+        MovingPlatform
+    }
+
     [SerializeField] private Animator animator;
     [SerializeField] private ButtonType buttonType = ButtonType.Hold;
+    [SerializeField] private ButtonBehaviourTarget behaviourTarget = ButtonBehaviourTarget.Door;
 
     public bool backtoup = true;
     private bool isPressed = false;
 
+    [Header("Door Settings")]
+    [SerializeField] private Door currentDoor;
+
+    [Header("Optional Moving Platform Button Activate")]
+    //[SerializeField] private GameObject movingPlatform;
+    [SerializeField]private OptimizedMovingPlatform movingPlatformScript;
+    
     private void OnTriggerEnter(Collider other)
     {
         if (buttonType == ButtonType.PressOnce && !isPressed)
         {
-            currentDoor.AddPressureSwitch(this);
+            //currentDoor.AddPressureSwitch(this);
             animator.SetBool("Down", true);
             isPressed = true;
+            ActivateTarget();
         }
+
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (buttonType == ButtonType.Hold)
         {
-            currentDoor.AddPressureSwitch(this);
+            //currentDoor.AddPressureSwitch(this);
             animator.SetBool("Down", true);
+            ActivateTarget();
         }
     }
 
@@ -38,13 +54,42 @@ public class PressureSwitch : MonoBehaviour
     {
         if (buttonType == ButtonType.Hold)
         {
-            currentDoor.RemovePressureSwitch(this);
+            //currentDoor.RemovePressureSwitch(this);
             animator.SetBool("Down", false);
+            DeactivateTarget();
         }
     }
 
     public void SetButtonDown()
     {
         animator.SetBool("Down", true);
+    }
+
+    private void ActivateTarget()
+    {
+        switch (behaviourTarget)
+        {
+            case ButtonBehaviourTarget.Door:
+                currentDoor?.AddPressureSwitch(this);
+                break;
+            case ButtonBehaviourTarget.MovingPlatform:
+                if (movingPlatformScript != null)
+                    movingPlatformScript.enabled = true;
+                break;
+        }
+    }
+
+    private void DeactivateTarget()
+    {
+        switch (behaviourTarget)
+        {
+            case ButtonBehaviourTarget.Door:
+                currentDoor?.RemovePressureSwitch(this);
+                break;
+            //case ButtonBehaviourTarget.MovingPlatform:
+            //    if (movingPlatformScript != null)
+            //        movingPlatformScript.enabled = false;
+            //    break;
+        }
     }
 }
