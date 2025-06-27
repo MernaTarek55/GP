8524ca0b0c6b599add4d1fb350ed6ac32dd69ac1
mohamed alaa <m-aa2001@hotmail.com>
2025-06-27 12:23:43 +0300

    edit turret slide

--- Assets/beyplade_Presentation.cs
+++ Assets/beyplade_Presentation.cs
@@ -3,12 +3,11 @@ using UnityEngine;
 public class beyplade_Presentation : MonoBehaviour
 {
     private Vector3 sPos;
-    private Animator animator;
+    [SerializeField]private Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         sPos = transform.position;
-        animator = GetComponent<Animator>();
     }
     private void OnEnable()
     {
